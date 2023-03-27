5d3386a87afb34ae4732f0a6e65414615ac27986
Nero Kwok <nerokwok96@yahoo.com>
2023-03-27 22:01:36 +0800

    abc

--- server.ts
+++ server.ts
@@ -5,9 +5,12 @@ import path from "path";
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(
   expressSession({
-    secret: "gufyeahjkfuiahgvaenrfa",
+    secret: "very secret",
     saveUninitialized: true,
     resave: true,
   })
@@ -15,31 +18,20 @@ app.use(
 
 declare module "express-session" {
   interface SessionData {
-    counter?: number;
+    isLoggedIn?: boolean;
   }
 }
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  if (isNaN(req.session.counter!)) {
-    req.session.counter = 0;
-  } else {
-    req.session.counter! += 1;
-  }
-  const datetime = new Date();
-  console.log(
-    `${req.session.id} (${req.session.counter}): [${datetime.getFullYear()}-${
-      datetime.getMonth() + 1
-    }-${datetime.getDate()} ${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}] Request ${
-      req.url
-    }`
-  );
-  next();
-});
+// Section xxx: Route Handlers
+import { authRoutes } from "./routers/authRoutes";
+import { userRoutes } from "./routers/memoRoutes";
 
-app.use(express.static("publiccopy"));
+app.use(express.static("public"));
 
-app.use((req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "publicccopy", "404.html"));
+
+// Section xxx: Error Handling
+app.use((_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "public", "404.html"));
 });
 
 const PORT = 8080;
