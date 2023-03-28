2d0b90989ff5d075122fad6e0029f6c31d926656
Tyson Wong <fung60726589@gmail.com>
2023-03-28 12:21:58 +0800

    28/3 finsih formidable

--- server.ts
+++ server.ts
@@ -8,68 +8,15 @@ import formidable from "formidable";
 import IncomingForm from "formidable/Formidable";
 
 const app = express();
-// <<<<<<< HEAD
-// const USER_JSON_PATH = path.join(__dirname, "public", "data", "users.json");
-// const UPLOAD_JSON_PATH = path.join(
-//   __dirname,
-//   "private",
-//   "data",
-//   "uploads.json"
-// );
-// =======
+
 const USER_JSON_PATH = path.join(__dirname, "data", "users.json");
 const PARTYROOM_JSON_PATH = path.join(__dirname, "data", "partyrooms.json");
-// >>>>>>> 7d6b4208fdca4c9ded00f0b9840016a108a54a31
 
 interface User {
   name: string;
   password: string;
 }
 
-// <<<<<<< HEAD
-// interface UploadData {
-//   id: number;
-//   ownerName: string;
-//   phoneNum: number;
-//   price: number;
-//   area: number;
-//   capacity: number;
-//   style: string;
-//   venue: string;
-//   intro: string;
-//   equipment: string;
-//   image?: string;
-// }
-
-// const uploadDir = "uploads";
-// fs.mkdirSync(uploadDir, { recursive: true });
-
-// const form = formidable({
-//   uploadDir,
-//   keepExtensions: false,
-//   maxFiles: 1,
-//   maxFileSize: 1024 ** 2 * 200, // the default limit is 1KB * 200
-//   filter: (part) => part.mimetype?.startsWith("image/") || false,
-//   filename: (_originalName, _originalExt, part) => {
-//     const fieldName = part.name;
-//     const timestamp = Date.now();
-//     const ext = part.mimetype?.split("/").pop();
-//     return `${fieldName}-${timestamp}.${ext}`;
-//   },
-// });
-
-// export function formParsePromise(form: IncomingForm, req: express.Request) {
-//   return new Promise<{ fields: formidable.Fields; files: formidable.Files }>(
-//     (resolve, reject) => {
-//       form.parse(req, (err, fields, files) => {
-//         if (err) reject(err);
-//         else resolve({ fields, files });
-//       });
-//     }
-//   );
-// }
-
-// =======
 interface Partyroom {
   id: number;
   name: string;
@@ -111,7 +58,6 @@ export function partyroomFormPromise(form: IncomingForm, req: express.Request) {
 }
 
 // Section xxx: Basic Middleware //
-// >>>>>>> 7d6b4208fdca4c9ded00f0b9840016a108a54a31
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
@@ -226,101 +172,6 @@ const guardMiddleware = (req: Request, res: Response, next: NextFunction) => {
   }
 };
 app.use(guardMiddleware, express.static("private"));
-
-/* 
----------------------------------------------upload users data------------------------------------------------------------------------
- */
-
-// import formidable from "formidable";
-// import IncomingForm from "formidable/Formidable";
-// import fs from "fs";
-
-// app.post("/uploadData", async (req, res) => {
-//   const { fields, files } = await formParsePromise(form, req);
-//   //係formidable要加番formParsePromise呢個function
-
-//   //upload info
-//   const ownerName = fields.ownerName as string;
-//   if (!ownerName) {
-//     res.status(400).json({ message: "missing ownerName" });
-//     return;
-//   }
-
-//   const phoneNum = parseInt(
-//     Array.isArray(fields.phoneNum) ? fields.phoneNum[0] : fields.phoneNum
-//   );
-//   if (isNaN(phoneNum)) {
-//     res.status(400).json({ message: "invalid phoneNum" });
-//     return;
-//   }
-
-//   const price = parseFloat(
-//     Array.isArray(fields.price) ? fields.price[0] : fields.price
-//   );
-//   if (isNaN(price)) {
-//     res.status(400).json({ message: "invalid price" });
-//     return;
-//   }
-
-//   const area = parseFloat(
-//     Array.isArray(fields.area) ? fields.area[0] : fields.area
-//   );
-//   if (isNaN(area)) {
-//     res.status(400).json({ message: "invalid area" });
-//     return;
-//   }
-
-//   const capacity = parseInt(fields.capacity as string);
-//   if (isNaN(capacity)) {
-//     res.status(400).json({ message: "invalid capacity" });
-//     return;
-//   }
-
-//   const style = fields.style as string;
-//   if (!style) {
-//     res.status(400).json({ message: "missing style" });
-//     return;
-//   }
-
-//   const venue = fields.venue as string;
-//   if (!venue) {
-//     res.status(400).json({ message: "missing venue" });
-//     return;
-//   }
-
-//   const intro = fields.intro as string;
-//   if (!intro) {
-//     res.status(400).json({ message: "missing intro" });
-//     return;
-//   }
-
-//   const equipment = fields.equipment as string;
-//   if (!equipment) {
-//     res.status(400).json({ message: "missing equipment" });
-//     return;
-//   }
-
-//   const imageFilename = (files.image as formidable.File | undefined)
-//     ?.newFilename;
-
-//   const uploadData: UploadData[] = jsonfile.readFileSync(UPLOAD_JSON_PATH);
-//   uploadData.push({
-//     id: uploadData.length + 1,
-//     ownerName,
-//     phoneNum,
-//     price,
-//     area,
-//     capacity,
-//     style,
-//     venue,
-//     intro,
-//     equipment,
-//     image: imageFilename,
-//   });
-//   jsonfile.writeFileSync(UPLOAD_JSON_PATH, uploadData, { spaces: 2 });
-
-//   res.json({ message: "success" });
-// });
 
 // Section xxx: Error Handling
 app.use((_req: Request, res: Response) => {
