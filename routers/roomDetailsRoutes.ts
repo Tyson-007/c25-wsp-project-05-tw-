dde2c58310e583712adc68f3d242d46559573689
Nero Kwok <nerokwok96@yahoo.com>
2023-03-30 12:46:39 +0800

    abc

--- routers/roomDetailsRoutes.ts
+++ routers/roomDetailsRoutes.ts
@@ -4,7 +4,7 @@ import type { Request, Response } from "express";
 
 export const roomDetailsRoutes = express.Router();
 
-roomDetailsRoutes.get("/:pid", getRoomDetails);
+roomDetailsRoutes.get("/:pid", getRoomDetails, geteqDetails);
 
 async function getRoomDetails(req: Request, res: Response) {
   try {
@@ -14,6 +14,21 @@ async function getRoomDetails(req: Request, res: Response) {
       return;
     }
     const resultQuery = await dbClient.query(/*SQL*/ `SELECT * FROM partyrooms WHERE id = $1`, [roomId]);
+    res.json(resultQuery.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "internal server error" });
+  }
+}
+
+async function geteqDetails(req: Request, res: Response) {
+  try {
+    const equipmentId = +req.params.pid;
+    if (isNaN(equipmentId)) {
+      res.status(400).json({ message: "invalid room id" });
+      return;
+    }
+    const resultQuery = await dbClient.query(/*SQL*/ `SELECT * FROM equipments WHERE id = $1`, [equipmentId]);
     res.json(resultQuery.rows[0]);
   } catch (err) {
     console.log(err);
