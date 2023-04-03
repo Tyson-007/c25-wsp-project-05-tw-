7ccd7e49e93850aa312fb57a7fd4c20035a32a02
John Doe <123931543+onu941@users.noreply.github.com>
2023-04-03 17:23:39 +0800

    tentative finished booking

--- routers/bookingDetailsRoutes.ts
+++ routers/bookingDetailsRoutes.ts
@@ -8,13 +8,13 @@ import type { Request, Response } from "express";
 
 export const bookingDetailsRoutes = express.Router();
 
-bookingDetailsRoutes.get("/:pid", getBookingDetails);
+bookingDetailsRoutes.get("/:bid", getBookingDetails);
 
 // get details for one booking
 async function getBookingDetails(req: Request, res: Response) {
   try {
     const bookingId = +req.params.bid;
-    // console.log(bookingId);
+    console.log("booking ID: " + bookingId);
 
     // if (isNaN(bookingId)) {
     //   res.status(400).json({ message: "invalid booking id" });
@@ -26,7 +26,7 @@ async function getBookingDetails(req: Request, res: Response) {
         /*SQL*/ `SELECT bookings.id, users.name AS owner, partyrooms.name AS room_name, start_at, finish_at, participants, special_req, venue, partyrooms.phone_no FROM bookings INNER JOIN partyrooms ON bookings.partyroom_id = partyrooms.id INNER JOIN users ON partyrooms.user_id = users.id WHERE bookings.id = $1`,
         [bookingId]
       )
-    ).rows;
+    ).rows[0];
 
     console.log(resultQuery);
 
