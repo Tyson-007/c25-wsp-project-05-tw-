93e3958587ac36c36fcdf107947f3aa76b3bc0e1
Nero Kwok <nerokwok96@yahoo.com>
2023-04-06 14:57:28 +0800

    abc

--- private/js/partyrooms_details.js
+++ private/js/partyrooms_details.js
@@ -73,7 +73,7 @@ async function uploadBookingInfo() {
     const finish_at = form.finish_at.value;
     const participants = form.participants.value;
     const special_req = form.special_req.value;
-
+  
     const res = await fetch(`/user/booking/${urlSearchParams.get("pid")}`, {
       method: "POST",
       headers: {
@@ -82,6 +82,9 @@ async function uploadBookingInfo() {
       body: JSON.stringify({ start_at, finish_at, participants, special_req }),
     });
     // const booking_details = await res.json();
+    if (start_at < finish_at){
+      console.log("hihihi");
+    }
     if (res.status === 200) {
       window.location = `/users.html`;
       alert("success");
