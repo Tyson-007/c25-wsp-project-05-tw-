b3e782054f8f4ea65200d8311280a383338e3d06
John Doe <123931543+onu941@users.noreply.github.com>
2023-04-09 21:13:32 +0800

    minor fixes

--- private/js/users.js
+++ private/js/users.js
@@ -105,7 +105,7 @@ async function getAllRooms() {
             partyroom.id
           }" class="result">${card_image}</a>
           <div class="card-body">
-            <div class="card-title result"><h4>${partyroom.name}</h4></div>
+            <div class="card-title result"><h5>${partyroom.name}</h5></div>
             <div class="card-text result">
               <p>${partyroom.venue}</p>
               最多${partyroom.capacity}人&nbsp;&nbsp;|&nbsp;&nbsp;${
@@ -168,7 +168,7 @@ async function getMyRooms() {
         <div class="card result w-75 partyroom-card mb-3 justify-content-center" data-id="${partyroom.id}">
           <a href= "/partyrooms_details.html?pid=${partyroom.id}" class="result">${card_image}</a>
           <div class="card-body">
-            <div class="card-title result"><h4>${partyroom.name}</h4></div>
+            <div class="card-title result"><h5>${partyroom.name}</h5></div>
             <div class="card-text result"><p>${partyroom.venue}</p>
             最多${partyroom.capacity}人&nbsp;&nbsp;|&nbsp;&nbsp;${partyroom.style}
             </div>
@@ -206,7 +206,7 @@ async function getOthersRooms() {
         <div class="card result w-75 partyroom-card mb-3 justify-content-center" data-id="${partyroom.id}">
           <a href= "/partyrooms_details.html?pid=${partyroom.id}" class="result">${card_image}</a>
           <div class="card-body">
-            <div class="card-title result"><h4>${partyroom.name}</h4></div>
+            <div class="card-title result"><h5>${partyroom.name}</h5></div>
             <div class="card-text result"><p>${partyroom.venue}</p>
             最多${partyroom.capacity}人&nbsp;&nbsp;|&nbsp;&nbsp;${partyroom.style}
             </div>
@@ -344,7 +344,7 @@ function instanceSearch() {
                 partyroom.id
               }" class="result">${card_image}</a>
               <div class="card-body">
-                <div class="card-title result"><h4>${partyroom.name}</h4></div>
+                <div class="card-title result"><h5>${partyroom.name}</h5></div>
                 <div class="card-text result">${partyroom.venue}</div>
               </div>
               <div class="result card-footer d-flex justify-content-around" data-id=${
